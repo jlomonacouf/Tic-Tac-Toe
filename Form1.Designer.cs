4b7f65a3dfea7b056f0fd6c53cb606b0d423a820
Justin <jlomonaco@ufl.edu>
2018-10-16 18:45:45 -0400

    Uploaded Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace tictactoe
+{
+    partial class board_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewStatisticsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutTicTacToeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.board_Panel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.newGame_Button = new System.Windows.Forms.Button();
+            this.turn_Label = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(538, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewStatisticsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.aboutTicTacToeToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(92, 38);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // viewStatisticsToolStripMenuItem
+            // 
+            this.viewStatisticsToolStripMenuItem.Name = "viewStatisticsToolStripMenuItem";
+            this.viewStatisticsToolStripMenuItem.Size = new System.Drawing.Size(301, 38);
+            this.viewStatisticsToolStripMenuItem.Text = "View Statistics";
+            this.viewStatisticsToolStripMenuItem.Click += new System.EventHandler(this.viewStatisticsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(298, 6);
+            // 
+            // aboutTicTacToeToolStripMenuItem
+            // 
+            this.aboutTicTacToeToolStripMenuItem.Name = "aboutTicTacToeToolStripMenuItem";
+            this.aboutTicTacToeToolStripMenuItem.Size = new System.Drawing.Size(301, 38);
+            this.aboutTicTacToeToolStripMenuItem.Text = "About Tic Tac Toe";
+            // 
+            // board_Panel
+            // 
+            this.board_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.board_Panel.Location = new System.Drawing.Point(0, 42);
+            this.board_Panel.Name = "board_Panel";
+            this.board_Panel.Size = new System.Drawing.Size(538, 341);
+            this.board_Panel.TabIndex = 1;
+            this.board_Panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.board_Panel_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.newGame_Button);
+            this.panel1.Controls.Add(this.turn_Label);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 383);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(538, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // newGame_Button
+            // 
+            this.newGame_Button.Font = new System.Drawing.Font("Arial", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGame_Button.Location = new System.Drawing.Point(385, 11);
+            this.newGame_Button.Name = "newGame_Button";
+            this.newGame_Button.Size = new System.Drawing.Size(141, 45);
+            this.newGame_Button.TabIndex = 1;
+            this.newGame_Button.Text = "New Game";
+            this.newGame_Button.UseVisualStyleBackColor = true;
+            this.newGame_Button.Click += new System.EventHandler(this.newGame_Button_Click);
+            // 
+            // turn_Label
+            // 
+            this.turn_Label.AutoSize = true;
+            this.turn_Label.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turn_Label.Location = new System.Drawing.Point(12, 24);
+            this.turn_Label.Name = "turn_Label";
+            this.turn_Label.Size = new System.Drawing.Size(102, 32);
+            this.turn_Label.TabIndex = 0;
+            this.turn_Label.Text = "Turn: X";
+            // 
+            // board_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 451);
+            this.Controls.Add(this.board_Panel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "board_Form";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.board_Form_Load);
+            this.Shown += new System.EventHandler(this.board_Form_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel board_Panel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button newGame_Button;
+        private System.Windows.Forms.Label turn_Label;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewStatisticsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutTicTacToeToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class board_Form : Form
+    {
+        public board_Form()
+        {
+            InitializeComponent();
+        }
+
+        Graphics boardGraphics;
+
+        int[] markedPositions = new int[9]; // 0 = nothing, 1 = X, 2 = O
+
+        bool xTurn = true;
+        bool gameOver = false;
+        int turnCount = 0, xWinsCount = 0, oWinsCount = 0, tieCount = 0;
+
+        Pen linePen = new Pen(Color.Black, 1);
+        Pen xPen = new Pen(Color.Red, 10);
+        Pen oPen = new Pen(Color.Blue, 10);
+
+        private void board_Form_Load(object sender, EventArgs e)
+        {
+            boardGraphics = board_Panel.CreateGraphics();
+        }
+
+        private void newGame_Button_Click(object sender, EventArgs e)
+        {
+            createBoard();
+        }
+
+        private void board_Panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            activateTurn(e.Location);
+        }
+
+        private void createBoard()
+        {
+            turnCount = 0;
+            gameOver = false;
+
+            boardGraphics.Clear(board_Panel.BackColor);
+            turn_Label.Text = (xTurn) ? "Turn: X" : "Turn: O";
+
+            int boardWidth = board_Panel.Width;
+            int boardHeight = board_Panel.Height;
+
+            boardGraphics.DrawLine(linePen, new Point(boardWidth / 3, 10), new Point(boardWidth / 3, boardHeight - 10));
+            boardGraphics.DrawLine(linePen, new Point(2 * boardWidth / 3, 10), new Point(2 * boardWidth / 3, boardHeight - 10));
+
+            boardGraphics.DrawLine(linePen, new Point(10, boardHeight / 3), new Point(boardWidth - 10, boardHeight / 3));
+            boardGraphics.DrawLine(linePen, new Point(10, 2 * boardHeight / 3), new Point(boardWidth - 10, 2 * boardHeight / 3));
+
+            Array.Clear(markedPositions, 0, markedPositions.Length);
+        }
+
+        private void activateTurn(Point position)
+        {
+            int column = getColumn(position.X);
+            int row = getRow(position.Y);
+            int arrayIndex = column + (row * 3);
+
+            turnCount++;
+
+            if (markedPositions[arrayIndex] == 0 && !gameOver)
+            {
+                markedPositions[arrayIndex] = (xTurn) ? 1 : 2;
+                drawOnBoard(new Point(row, column));
+                xTurn = !xTurn;
+                turn_Label.Text = (xTurn) ? "Turn: X" : "Turn: O";
+            }
+            else
+            {
+                MessageBox.Show((gameOver) ? "Game Already Finished" : "Already Marked!", "Error");
+                return;
+            }
+
+            #region Game Logic
+            if (turnCount >= 5)
+            {
+                bool? xWin = null;
+
+                for (int mark = 0; mark < markedPositions.Length; mark++)
+                {
+                    if (markedPositions[mark] != 0)
+                    {
+                        if (mark <= 2)
+                        {
+                            if (markedPositions[mark] == markedPositions[mark + 3] && markedPositions[mark] == markedPositions[mark + 6])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                            else if (markedPositions[mark] == markedPositions[mark + 3] && markedPositions[mark] == markedPositions[mark + 6])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                        }
+                        else if (mark <= 6)
+                        {
+                            if ((mark % 3) == 0 && markedPositions[mark] == markedPositions[mark + 1] && markedPositions[mark] == markedPositions[mark + 2])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                        }
+
+                        if (mark == 0)
+                        {
+                            if (markedPositions[mark] == markedPositions[mark + 4] && markedPositions[mark] == markedPositions[mark + 8])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                            else if (markedPositions[mark] == markedPositions[mark + 1] && markedPositions[mark] == markedPositions[mark + 2])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                        }
+                        else if (mark == 2)
+                        {
+                            if (markedPositions[mark] == markedPositions[mark + 2] && markedPositions[mark] == markedPositions[mark + 4])
+                            {
+                                xWin = (!xTurn) ? true : false;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (xWin == null && turnCount == 9) //Check if there is a draw
+                {
+                    giveDraw();
+                }
+
+                if (xWin != null)
+                {
+                    giveWin(Convert.ToBoolean(xWin));
+                }
+            }
+            #endregion
+
+        }
+
+        private void drawOnBoard(Point boardPosition)
+        {
+            Point startingPoint = new Point((boardPosition.Y * (board_Panel.Width / 3)) + 10, (boardPosition.X * (board_Panel.Height / 3)) + 10);
+            Point endPoint = new Point(startingPoint.X + (board_Panel.Width / 3) - 15, startingPoint.Y + (board_Panel.Height / 3) - 15);
+            Rectangle ellipseBounds = new Rectangle(startingPoint.X, startingPoint.Y, endPoint.X - startingPoint.X, endPoint.Y - startingPoint.Y);
+            if (xTurn)
+            {
+                boardGraphics.DrawLine(xPen, startingPoint, endPoint);
+                boardGraphics.DrawLine(xPen, new Point(startingPoint.X, endPoint.Y), new Point(endPoint.X, startingPoint.Y));
+            }
+            else
+            {
+                boardGraphics.DrawEllipse(oPen, ellipseBounds);
+            }
+        }
+
+        private int getRow(int yLocation)
+        {
+            int boardHeight = board_Panel.Height;
+
+            if (yLocation < boardHeight / 3)
+            {
+                return 0;
+            }
+            else if (yLocation > 2 * boardHeight / 3)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private int getColumn(int xLocation)
+        {
+            int boardWidth = board_Panel.Width;
+
+            if (xLocation < boardWidth / 3)
+            {
+                return 0;
+            }
+            else if (xLocation > 2 * boardWidth / 3)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private void board_Form_Shown(object sender, EventArgs e)
+        {
+            createBoard();
+        }
+
+        private void giveWin(bool xWin)
+        {
+            turn_Label.Text = "Game Over";
+            gameOver = true;
+            if (xWin)
+            {
+                tictactoe.Properties.Settings.Default.xWinsTotal++;
+                xWinsCount++;
+                MessageBox.Show("X's win!", "Game Over");
+            }
+            else
+            {
+                tictactoe.Properties.Settings.Default.oWinsTotal++;
+                oWinsCount++;
+                MessageBox.Show("O's win!", "Game Over");
+            }
+        }
+
+        private void giveDraw()
+        {
+            turn_Label.Text = "Game Over";
+            gameOver = true;
+            tictactoe.Properties.Settings.Default.tieGamesTotal++;
+            tieCount++;
+            MessageBox.Show("Draw", "Game Over");
+        }
+
+        private void viewStatisticsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CURRENT SESSION:\nX Wins: " + xWinsCount + "\nO Wins: " + oWinsCount + "\nDraw Games: " + tieCount + "\nTotal Games: " + (xWinsCount + oWinsCount + tieCount) + "\n\nALL SESSIONS:\nX Wins: " + tictactoe.Properties.Settings.Default.xWinsTotal + "\nO Wins: " + tictactoe.Properties.Settings.Default.oWinsTotal + "\nDraw Games: " + tictactoe.Properties.Settings.Default.tieGamesTotal + "\nTotal Games: " + (tictactoe.Properties.Settings.Default.xWinsTotal + tictactoe.Properties.Settings.Default.oWinsTotal + tictactoe.Properties.Settings.Default.tieGamesTotal), "Game Statistics");
+        }
+    }
+}
